0031db56e92620beb1ea452815e5835917e8c676
SantiagoZarate <santiago2001zarate@gmail.com>
2025-01-03 23:03:26 -0300

    chore: set up shadcn

--- src/routes/persons.lazy.tsx
+++ src/routes/persons.lazy.tsx
@@ -1,3 +1,4 @@
+import { Input } from "@/components/ui/input";
 import { GetPersonResponse } from "@/interfaces/person.interface";
 import { useQuery } from "@tanstack/react-query";
 import { createLazyFileRoute } from "@tanstack/react-router";
@@ -26,13 +27,10 @@ function RouteComponent() {
     <section className="flex items-center justify-center p-8 flex-col bg-red-300 w-full mx-auto min-w-80">
       <h1>{count}</h1>
       <p>My first extension using wxt!</p>
-      <ul className="flex flex-col divide-y">
-        {data?.map((user) => (
-          <li className="p-2" key={user.id.value}>
-            {user.name.first}
-          </li>
-        ))}
-      </ul>
+      <form action="" className="flex flex-col gap-2">
+        <label htmlFor="">Buscar producto</label>
+        <Input />
+      </form>
     </section>
   );
 }
