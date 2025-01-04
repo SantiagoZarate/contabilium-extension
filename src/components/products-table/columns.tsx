38b70bc9055bee7806bb88a963bf38611eac1de6
Santiago Zarate <santiago2001zarate@gmail.com>
2025-01-04 15:06:02 -0300

    feat: add table with sortabla items by price

---
+++ src/components/products-table/columns.tsx
@@ -0,0 +1,40 @@
+"use client"
+
+import { ItemDTO } from "@/api/contabilium/dto/itemDTO"
+import { ColumnDef } from "@tanstack/react-table"
+import { ArrowUpDown } from "lucide-react"
+import { Button } from "../ui/button"
+
+export const productsColumns: ColumnDef<ItemDTO>[] = [
+  {
+    accessorKey: "id",
+    header: "Id",
+  },
+  {
+    accessorKey: "name",
+    header: "Nombre",
+  },
+  {
+    accessorKey: "price",
+    cell: ({ row }) => {
+      const amount = parseFloat(row.getValue("price"))
+      const formatted = new Intl.NumberFormat("en-US", {
+        style: "currency",
+        currency: "USD",
+      }).format(amount)
+
+      return <div className="text-right font-medium">{formatted}</div>
+    },
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Precio
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      )
+    },
+  },
+]
--- src/routes/marcas.lazy.tsx
+++ src/routes/marcas.lazy.tsx
@@ -1,5 +1,7 @@
 import { contabiliumApi } from '@/api/contabilium/api'
 import { CategorySelector } from '@/components/category-selector/category-selector'
+import { productsColumns } from '@/components/products-table/columns'
+import { DataTable } from '@/components/products-table/products-table'
 import { Button } from '@/components/ui/button'
 import { useQuery } from '@tanstack/react-query'
 import { createLazyFileRoute } from '@tanstack/react-router'
@@ -13,7 +15,7 @@ export const Route = createLazyFileRoute('/marcas')({
 function RouteComponent() {
   const [value, setValue] = useState<string>('')
   const lastBrand = useRef<string>('')
-  const {data, isLoading, isError, refetch} = useQuery({
+  const {data, isLoading, isError, refetch, isFetching} = useQuery({
     queryKey: ['products'],
     queryFn: () => contabiliumApi.getProducts({ page : 1 }),
     enabled: false
@@ -26,22 +28,17 @@ function RouteComponent() {
 
   return (
     <section className="flex flex-col gap-4 items-center p-6 min-h-dvh">
+      <h1 className='font-bold'>
+        Buscar productos por marca
+      </h1>
       <header className='flex gap-4'>
         <CategorySelector onUpdateValue={(value) => setValue(value)} value={value} />
         <Button disabled={!value} onClick={handleSearchProducts}>Buscar</Button>
       </header>
       {isError && <p>Error al obtener los prouctos de la marca {lastBrand.current}</p>}
-      {!isError && isLoading && <LoaderCircleIcon className='animate-spin'/>}
-      {!isError && !isLoading && 
-        <ul className='flex flex-col divide-y'>
-          {
-            data?.map(item => 
-              <li className='p-2 uppercase' key={item.id}>
-                {item.name}
-              </li>
-            )
-          }
-        </ul>
+      {!isError && isFetching && <LoaderCircleIcon className='animate-spin'/>}
+      {!isError && !isFetching && data?.length &&
+        <DataTable columns={productsColumns} data={data} />
       }
     </section>
   )
